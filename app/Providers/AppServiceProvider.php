0e0a39784c5952758f3b146400b7bbd0264fea77
me@mmp.m3 <TWR-****-****-****-7F47>
2024-06-21 11:33:12 -0700

    remove permission allow window

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\Passport;
+use App\Models\Passport\Client;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment() !== 'local') {
+            URL::forceScheme('https');
+        }
+        Passport::useClientModel(Client::class);
         //
     }
 }
