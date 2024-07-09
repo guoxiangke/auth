b0ab939e09b53467011e6431dd88be6846283166
me@mbp.m3 <TWR-****-****-****-7F47>
2024-07-09 11:39:08 -0700

    v1 v2 test

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Log;
 
 Route::prefix('v1')->get('/user', function (Request $request) {
+    Log::error('v1-requestAll', $request->all());
+    Log::error('v1-auth.user', [auth()->user()]);
     return $request->user();
 })->middleware('auth:api');
+
+Route::prefix('v2')->get('/user', function (Request $request) {
+    Log::error('v2-requestAll', $request->all());
+    Log::error('v2-auth.user', [auth()->user()]);
+    return $request->user();
+});
