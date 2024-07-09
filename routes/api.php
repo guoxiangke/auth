b01a62af9105b76a0bff5e74d7b826eb8b6e7e2d
me@mbp.m3 <TWR-****-****-****-7F47>
2024-07-09 13:34:09 -0700

    done

--- routes/api.php
+++ routes/api.php
@@ -5,17 +5,9 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Log;
 
 Route::prefix('v1')->get('/user', function (Request $request) {
-    Log::error('v1-requestAll', $request->all());
-    Log::error('v1-auth.user', [auth()->user()]);
     return $request->user();
 })->middleware('auth:api');
 
-Route::prefix('v2')->get('/user', function (Request $request) {
-    Log::error('v2-requestAll', $request->all());
-    Log::error('v2-auth.user', [auth()->user()]);
-    return $request->user();
-});
-
 Route::prefix('v1')->get('/logs', function (Request $request) {
-    return file_get_contents('/tmp/laravel.log');
+    return "<pre>" . file_get_contents('/tmp/laravel.log');
 });
