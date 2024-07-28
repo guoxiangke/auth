c567544ebea693437067ebed8e5caa4b1aa1690d
me@mbp.m3 <TWR-****-****-****-7F47>
2024-07-28 09:41:56 -0700

    debug

--- app/Http/Controllers/WeixinController.php
+++ app/Http/Controllers/WeixinController.php
@@ -51,7 +51,7 @@ class WeixinController extends Controller
             Auth::loginUsingId($user->id, true);//自动登入！
         }
         $user->setMeta('wxid', $socialId);
-        $user->setMeta('weixin', $socialUser->user['attributes']);
+        $user->setMeta('weixin', $socialUser->attributes);
         Log::error(__CLASS__, [$avatar, $socialUser]);
         return Redirect::intended('dashboard');
     }
