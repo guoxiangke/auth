770dfbca76f92f22af2444ddce72f5abb201bb7d
me@mbp.m3 <TWR-****-****-****-7F47>
2024-07-29 15:14:45 -0700

    add nickname and avatar

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 // use Laravel\Sanctum\HasApiTokens;
 use Laravel\Passport\HasApiTokens;
 use Plank\Metable\Metable;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
@@ -53,8 +55,33 @@ class User extends Authenticatable
      */
     protected $appends = [
         'profile_photo_url',
-    ];
+        'nickname',
+        'avatar',
+        'wxid',
+    ];    
+    
+    /**
+     * Get the cover.
+     * getNickNameAttribute
+     */
+    protected function nickname(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->name,
+        );
+    }
 
+    protected function wxid(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->getMeta('wxid','')
+        );
+    }
+
+    public function getAvatarAttribute()
+    {
+        return $this->profile_photo_path;
+    }
     /**
      * Get the attributes that should be cast.
      *
