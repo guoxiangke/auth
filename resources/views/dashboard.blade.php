bad99902d29f677f46150c6243f617633899ac80
me@mbp.m3 <TWR-****-****-****-7F47>
2025-05-17 20:40:47 -0700

    要使用完整服务

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,14 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
+            要使用完整服务，请授权访问您的头像和昵称
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-                <x-welcome />
+               <p>要使用完整服务，请授权访问您的头像和昵称</p>
             </div>
         </div>
     </div>
