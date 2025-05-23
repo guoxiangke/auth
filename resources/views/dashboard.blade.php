ba88c04f9650f1bdff1e53bdd0f86972fda87d4a
me@mbp.m3 <TWR-****-****-****-7F47>
2025-05-23 00:00:45 -0700

    授权页面

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,14 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            要使用完整服务，请授权访问您的头像和昵称
-        </h2>
-    </x-slot>
+@section('title', '授权访问，使用完整服务')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-               <p>要使用完整服务，请授权访问您的头像和昵称</p>
-            </div>
+<x-wechat-layout>
+    <div class="page">
+        <div class="page__bd page__bd_spacing">
+            <br/><br/>
+            <p class="page__desc"  style="text-align: center;">要使用完整服务，请授权访问您的头像和昵称</p>
+            <br/>
+            <a href="javascript:" role="button" title="等待中" class="weui-btn weui-btn_default">请点右下角↘</a>
+
+            <img  src="{{ asset('/images/pointer.webp') }}" style="transform: rotate(90deg); position: fixed; bottom: 0px;right: -50px;">
         </div>
     </div>
-</x-app-layout>
+</x-wechat-layout>
---
+++ resources/views/layouts/wechat.blade.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>
+          @if(isset($title)) {{ $title }}
+          @else
+            @yield('title')
+          @endif
+          | {{ config('app.name', 'Laravel') }}
+        </title>
+
+
+        <link rel="icon" type="image/png" sizes="16x16" href="https://wj.qq.com/favicon-16x16.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="https://wj.qq.com/favicon-16x16.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="https://wj.qq.com/favicon-32x32.png">
+        <link rel="mask-icon" href="https://wj.qq.com/safari-pinned-tab.svg" color="#5bbad5">
+        
+        <!-- Scripts -->
+        @vite(['resources/js/app.js'])
+
+        <!-- Styles -->
+        @livewireStyles
+
+        <!-- Fonts -->
+        <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0,viewport-fit=cover">
+        <meta name="wechat-enable-text-zoom-em" content="true">
+        <title>{{ config('app.name', 'Laravel') }}</title>
+        <link rel="stylesheet" href="https://res.wx.qq.com/t/wx_fed/weui-source/res/2.5.14/weui.css">
+        <style>
+            .weui-msg {
+                min-height: 100vh;
+            }
+            .page__desc {
+              margin-top: 4px;
+              color: var(--weui-FG-1);
+              text-align: left;
+              font-size: 14px;
+            }
+            a {
+              color: #999;
+            }
+            a:visited {
+              color: #666;
+            }
+        </style>
+
+        <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+
+        <!-- Scripts -->
+        @vite(['resources/js/app.js'])
+    </head>
+    <body ontouchstart class="body" data-weui-theme="light">
+        <div class="container" id="container">
+             {{ $slot }}
+        </div>
+        
+        <script type="text/javascript" src="https://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
+        <script src="https://res.wx.qq.com/t/wx_fed/cdn_libs/res/weui/1.2.8/weui.min.js"></script>
+
+        <script type="text/javascript">
+          function wxReady(callback) {
+            if (
+              typeof WeixinJSBridge === 'object' &&
+              typeof window.WeixinJSBridge.invoke === 'function'
+            ) {
+              callback()
+            } else {
+              document.addEventListener('WeixinJSBridgeReady', callback, false)
+            }
+          }
+          wxReady(function() {
+            WeixinJSBridge.invoke('getUserConfig', {}, function(res) {
+              if (res.isCareMode) {
+                document.body.setAttribute('data-weui-mode','care');
+              }
+            });
+            // 隐藏所有非基础按钮接口
+            wx.hideAllNonBaseMenuItem();
+            // 批量隐藏功能按钮接口
+            wx.hideMenuItems({
+            });
+
+            $('#close').click(function(){
+              wx.closeWindow();
+            });
+          });
+
+        </script>
+        @stack('modals')
+
+        @livewireScripts
+    </body>
+</html>
